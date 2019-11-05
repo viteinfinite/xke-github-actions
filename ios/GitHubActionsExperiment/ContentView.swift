9be927a521aec29137189cc898588e79c5590cc6
Simone Civetta <viteinfinite@gmail.com>
2019-11-05 15:03:57 +0100

    Add iOS project

---
+++ ios/GitHubActionsExperiment/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitHubActionsExperiment
+//
+//  Created by Simone Civetta on 05/11/2019.
+//  Copyright © 2019 Simone Civetta. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
